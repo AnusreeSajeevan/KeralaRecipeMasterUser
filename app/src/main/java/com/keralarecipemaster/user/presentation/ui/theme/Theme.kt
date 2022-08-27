2e7593f307d62215ff1e338f907040ad3b39dcd3
Anusree Sajeevan <anusreesajeevan@Anusrees-MacBook-Pro-2.local>
2022-08-27 21:05:42 +0530

    Change colors

--- app/src/main/java/com/keralarecipemaster/user/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/keralarecipemaster/user/presentation/ui/theme/Theme.kt
@@ -9,13 +9,13 @@ import androidx.compose.runtime.Composable
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = BritishAirways
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary = Bahia,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = BritishAirways
 
     /* Other default colors to override
     background = Color.White,
